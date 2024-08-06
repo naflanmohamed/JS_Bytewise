24e2d3fca0dfb44416ecf2bd8325ab97f59cf652
naflanmohamed <naflanmohammed611@gmail.com>
2024-08-07 00:55:33 +0500

    Update

---
+++ datatype&ecma.js
@@ -0,0 +1,15 @@
+"use strict"; // purpose of this line is to enable strict mode in the script
+// strict mode is a way to opt in to a restricted variant of JavaScript
+
+// alert(10 + 20); // 30   //reference error: alert is not defined in node.js  // in browser it will show an alert box with 30
+
+console.log(10 + 20); // 30  // 30 will be printed in the consoles
+
+// ------------------- Data Types -------------------
+
+console.log(typeof 10); // number
+console.log(typeof "Hello"); // string
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof NaN); // number
